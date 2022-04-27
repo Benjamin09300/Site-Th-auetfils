15faf447959e929f9d3cdb4986bad4031c48d901
Jasmin31000 <thomas.jasmin@hotmail.fr>
2022-04-28 00:04:32 +0200

    Mise en place gallery

---
+++ startbootstrap-agency-gh-pages/js/sciptgalery.js
@@ -0,0 +1,48 @@
+function photos1(){
+  document.getElementById('cphotos').src='assets/img/1.jpg';
+}
+
+function photos2(){
+  document.getElementById('cphotos').src='assets/img/2.jpg';
+}
+
+function photos3(){
+  document.getElementById('cphotos').src='assets/img/3.jpg';
+}
+
+function photos4(){
+  document.getElementById('cphotos').src='assets/img/4.jpg';
+}
+
+function photos5(){
+  document.getElementById('cphotos').src='assets/img/5.jpg';
+}
+
+function photos6(){
+  document.getElementById('cphotos').src='assets/img/6.jpg';
+}
+
+
+function photosv1(){
+  document.getElementById('vphotos').src='assets/img/1.jpg';
+}
+
+function photosv2(){
+  document.getElementById('vphotos').src='assets/img/2.jpg';
+}
+
+function photosv3(){
+  document.getElementById('vphotos').src='assets/img/3.jpg';
+}
+
+function photosv4(){
+  document.getElementById('vphotos').src='assets/img/4.jpg';
+}
+
+function photosv5(){
+  document.getElementById('vphotos').src='assets/img/5.jpg';
+}
+
+function photosv6(){
+  document.getElementById('vphotos').src='assets/img/6.jpg';
+}
