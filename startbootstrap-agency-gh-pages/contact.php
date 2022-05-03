4d00695bdcb6f36a7b8b05163063f807ce457963
Jasmin31000 <thomas.jasmin@hotmail.fr>
2022-05-03 04:34:43 +0200

    page contact

--- startbootstrap-agency-gh-pages/contact.php
+++ startbootstrap-agency-gh-pages/contact.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Théau & Fils</title>
+        <title>Théau & Fils : Contact</title>
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
@@ -27,32 +27,41 @@
   </script>
     </head>
     <body id="page-top">
-      <form id="contactForm">
-        <div class="row align-items-stretch mb-5">
-          <div class="col-md-6">
-            <div class="form-group">
-              <!-- Name input-->
-              <input class="form-control" id="name" type="text" placeholder="Nom *" />
-            </div>
-            <div class="form-group">
-              <!-- Email address input-->
-              <input class="form-control" id="email" type="email" placeholder="E-mail *" />
-            </div>
-            <div class="form-group mb-md-0">
-              <!-- Phone number input-->
-              <input class="form-control" id="phone" type="tel" placeholder="Téléphone *" />
-            </div>
+      <header class="masthead">
+          <div class="container">
+              <div class="masthead-heading text-uppercase">Nous Contactez !</div>
           </div>
-          <div class="col-md-6">
-            <div class="form-group form-group-textarea mb-md-0">
-              <!-- Message input-->
-              <textarea class="form-control" id="message" placeholder="Message *"></textarea>
+      </header>
+      <section class="page-section">
+        <div class="container">
+          <form id="contactForm" class="pad-for">
+            <div class="row align-items-stretch mb-5">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <!-- Name input-->
+                  <input class="form-control" id="name" type="text" placeholder="Nom *" name="name" />
+                </div>
+                <div class="form-group">
+                  <!-- Email address input-->
+                  <input class="form-control" id="email" type="email" placeholder="E-mail *" name="email" />
+                </div>
+                <div class="form-group mb-md-0">
+                  <!-- Phone number input-->
+                  <input class="form-control" id="phone" type="tel" placeholder="Téléphone *" name="tel" />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group form-group-textarea mb-md-0">
+                  <!-- Message input-->
+                  <textarea class="form-control" id="message" placeholder="Message *" name="mes"></textarea>
+                </div>
+              </div>
             </div>
-          </div>
+            <!-- Submit Button-->
+            <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Envoyer Message</button></div>
+          </form>
+          <!-- <?php
+          ?> -->
         </div>
-        <!-- Submit Button-->
-        <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Envoyer Message</button></div>
-      </form>
-      <?php
-      ?>
+      </section>
     </body>
