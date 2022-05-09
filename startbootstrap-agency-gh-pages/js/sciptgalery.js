7274ebd1f6a36568f173d77f67210f8a6c9ae914
Jasmin31000 <thomas.jasmin@hotmail.fr>
2022-05-09 12:27:58 +0200

    modif 09/05/22

--- startbootstrap-agency-gh-pages/js/sciptgalery.js
+++ startbootstrap-agency-gh-pages/js/sciptgalery.js
@@ -18,61 +18,61 @@ function photos3(){
 
 function photos4(){
   document.getElementById('cphotos').src='assets/img/portfolio/4.png';
-  var text = "Projet Campagne Saint Cyprien Port 66";
+  var text = "Projet campagne Saint Cyprien Port - 66";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos5(){
   document.getElementById('cphotos').src='assets/img/portfolio/5.png';
-  var text = "Projet campagne Saint cyprien village dep:66";
+  var text = "Projet campagne Saint cyprien village - 66";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos6(){
   document.getElementById('cphotos').src='assets/img/portfolio/6.png';
-  var text = "Projet Campagne Saint Cyprien Port 66";
+  var text = "Projet campagne Saint Cyprien Port - 66";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos7(){
   document.getElementById('cphotos').src='assets/img/portfolio/7.png';
-  var text = "Rénovation et agencement appartement Saint Cyprien 66";
+  var text = "Rénovation et agencement appartement Saint Cyprien - 66";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos8(){
   document.getElementById('cphotos').src='assets/img/portfolio/8.png';
-  var text = "Projet Vel'Occitanie - Voie verte Lavelanet 09";
+  var text = "Projet Vel'Occitanie - Voie verte Lavelanet - 09";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos9(){
   document.getElementById('cphotos').src='assets/img/portfolio/9.png';
-  var text = "Projet Vel'Occitanie - Voie verte Lavelanet 09";
+  var text = "Projet Vel'Occitanie - Voie verte Lavelanet - 09";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos10(){
   document.getElementById('cphotos').src='assets/img/portfolio/10.png';
-  var text = "Rénovation d'une grange";
+  var text = "Rénovation d'une grange - 09";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos11(){
   document.getElementById('cphotos').src='assets/img/portfolio/11.png';
-  var text = "Rénovation d'une grange";
+  var text = "Rénovation d'une grange - 09";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos12(){
   document.getElementById('cphotos').src='assets/img/portfolio/12.png';
-  var text = "Rénovation d'une grange";
+  var text = "Rénovation d'une grange - 09";
   document.getElementById('leg-img').innerHTML=text;
 }
 
 function photos13(){
   document.getElementById('cphotos').src='assets/img/portfolio/13.png';
-  var text = "Rénovation d'une grange";
+  var text = "Rénovation d'une grange - 09";
   document.getElementById('leg-img').innerHTML=text;
 }
 
